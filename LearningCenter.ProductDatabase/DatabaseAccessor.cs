5d4e8397507f1b8025cab0aea7f7504dd509438c
DESKTOP-E55BUP2\Turbo <mgkett@gmail.com>
2017-05-02 00:31:29 -0700

    y

---
+++ LearningCenter.ProductDatabase/DatabaseAccessor.cs
@@ -0,0 +1,23 @@
+﻿using LearningCenter.ProductDatabase;
+
+namespace LearningCenter.Repository
+{
+    public class DatabaseAccessor
+    {
+        private static readonly LearningCenterDBEntities entities;
+
+        static DatabaseAccessor()
+        {
+            entities = new LearningCenterDBEntities();
+            entities.Database.Connection.Open();
+        }
+
+        public static LearningCenterDBEntities Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
---
+++ LearningCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningCenter.Startup))]
+namespace LearningCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
